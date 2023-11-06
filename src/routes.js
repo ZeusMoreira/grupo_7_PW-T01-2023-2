5eedeaca9ce95e9f60c88d73c7be31a829d3c02e
ZeusMoreira <zeus.moreira111@gmail.com>
2023-11-06 18:04:06 -0400

    Finalização das etapas de registro, login e senha esquecida

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,18 @@
-import {BrowserRouter, Routes, Route } from "react-router-dom";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Unknown from "./components/Unknown";
 import LoginPage from "./pages/LoginPage/";
 import RegistroPage from "./pages/RegistroPage/";
+import HomePage from "./pages/HomePage";
+import PrivateRoute from "./PrivateRoute";
+import FreeRoute from "./FreeRoute";
 
 function RoutesApp() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<LoginPage />} />
+                <Route path="/" element={<FreeRoute Component={LoginPage} />} />
                 <Route path="/registro" element={<RegistroPage/>} />
-                <Route path="/home" element={<div></div>}/>
+                <Route path="/home" element={<PrivateRoute Component={HomePage} />} />
                 <Route path="*" element={<Unknown/>}></Route>
             </Routes>
         </BrowserRouter>
